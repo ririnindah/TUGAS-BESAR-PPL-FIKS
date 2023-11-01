f3015f817a5e5edc49c3b3921180a4b4822154dd
ririnindah <ririnkurin07@gmail.com>
2023-11-01 19:44:40 +0700

    import

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+// use Illuminate\Database\Seeder;
+use Illuminate\Support\LazyCollection;
 
 class DatabaseSeeder extends Seeder
 {
@@ -27,5 +30,42 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::disableQueryLog();
+
+        LazyCollection::make(function(){
+            $handle = fopen(public_path('storage/ImportData/data.csv'), 'r');
+            while(($line = fgetcsv($handle, 4096)) !== false){
+                $dataString = implode('; ', $line);
+                $row =  explode(';', $dataString);
+                yield $row;
+            }
+
+            fclose($handle);
+        })
+        ->skip(1)
+        ->chunk(1000)
+        ->each(function(LazyCollection $chunk){
+            $records = $chunk->map(function($row){
+                return [
+                    'email'=>$row[1],
+                    'password'=>$row[2],
+                    'role'=>$row[3],
+                    'nim'=>$row[4],
+                    'nama'=>$row[5],
+                    'status'=>$row[6],
+                    'departemen'=>$row[7],
+                    'fakultas'=>$row[8],
+                    'angkatan'=>$row[9],
+                    'jalur_masuk'=>$row[10],
+                    'provinsi'=>$row[11],
+                    'kabupaten'=>$row[12],
+                    'alamat'=>$row[13],
+                    'foto'=>$row[14],
+                    'dsn_id'=>$row[15],
+                ];
+            })->toArray();
+
+            DB::table('mahasiswas')->insert($records);
+        });
     }
 }
